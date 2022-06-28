fcfc34433ea039da6f61ccf673443ab231032b35
asahtik <asahtik@gmail.com>
2022-06-28 20:00:22 +0200

    Headers

--- manus_webshell/manipulator.py
+++ manus_webshell/manipulator.py
@@ -199,7 +199,8 @@ class ManipulatorMoveJointHandler(ManipulatorBlockingHandler):
             application, request, manipulator)
 
     def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Headers', '*')
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
 
     def options(self):
